81f35d7a840318b82dcb271295239e4cb9363150
남궁용진 <elelte991@tukorea.ac.kr>
2025-08-04 20:56:14 +0900

    static변수 main 밖에서

--- chap02/src/_02_Scanner/T02_Ex01.java
+++ chap02/src/_02_Scanner/T02_Ex01.java
@@ -3,6 +3,7 @@ package _02_Scanner;
 import java.util.Scanner;
 
 public class T02_Ex01 {
+	// static 변수를 사용할거면 main 밖에서 해야 됨
 
 	public static void main(String[] args) {
 		// 국어, 컴퓨터, 수학 점수를 입력받아 저장하고, 총점과 평균을 출력
@@ -24,8 +25,6 @@ public class T02_Ex01 {
 		
 		System.out.println("총점: "+total);
 		System.out.println("평균: "+avg);
-		
-		
 
 	}
 
