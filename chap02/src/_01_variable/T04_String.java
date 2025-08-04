97517a4c9e6cc7d70063008c045ff4a982e17544
남궁용진 <elelte991@tukorea.ac.kr>
2025-08-04 19:55:39 +0900

    Update T04_String.java

--- chap02/src/_01_variable/T04_String.java
+++ chap02/src/_01_variable/T04_String.java
@@ -5,8 +5,11 @@ public class T04_String {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String str1 = "813333호";
-		System.out.println(str1);
+		String str1 = "801호";
+		String str2 = "Java 풀스택";
+		System.out.println(str1+ " "+str2);
+		
+		System.out.println("강의실: 608호\n강좌명: \"자바 풀스택\"");
 	}
 
 }
